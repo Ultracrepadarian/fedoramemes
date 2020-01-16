7203b9aacbd64a9a45dc5a7e584570a6ebacc295
Ultracrepadarian <alec_tarr@yahoo.com>
2020-01-15 19:36:20 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,6 +5,6 @@ if (password == "rbrb"){
 alert ("Login successful");
 window.location = "success.html"; // Redirecting to other page.
 return false;
-} else {alert("Nope);}
+} else {alert("Nope);
 }
 }
