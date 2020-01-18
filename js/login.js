6e8a5a32d1bb36e0913346714b7e73c55243470b
Ultracrepadarian <alec_tarr@yahoo.com>
2020-01-18 13:49:33 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 // Below function Executes on click of login button.
 function validate(){
 var password = document.getElementById("password").value;
-if (password == "rbrb"){
+if (password == "192839182839"){
 alert ("Login successful");
 window.location = "success.html"; // Redirecting to other page.
 return false;
